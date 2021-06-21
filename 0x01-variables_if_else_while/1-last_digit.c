bf14786bc79d7788be380f34f5cd8de8a4eb4385
andres duque <totod8@hotmail.com>
2021-06-20 21:55:44 -0700

    mycommit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("last digit of %d is %d and is 0\n", n, k);
 	}
 
-	if ((n % 10) < 6 && (n != 0))
+	if ((n % 10) < 6 && ((n % 10) != 0))
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, k);
 	}
