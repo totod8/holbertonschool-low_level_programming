d44933ce9cebb678a0a8239a4f4a3dcd570d7944
andres duque <totod8@hotmail.com>
2021-06-21 20:02:54 -0700

    mycommit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
