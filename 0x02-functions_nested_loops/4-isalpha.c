04050ddbeb16e58188dc626ad95e7239ed103c71
andres duque <totod8@hotmail.com>
2021-06-24 16:10:15 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Write a function that checks for alphabetic character.
+ * @c: the caracter to print
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if (((c >= 65) && (c <= 90)) || ((c >= 97) && (c <= 122)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
