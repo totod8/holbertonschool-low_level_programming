398fc1033092cf6fb8fced4a394e6661fd18407f
andres duque <totod8@hotmail.com>
2021-06-24 11:04:09 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ * @c: variable
+ * Return: always 0
+ */
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
