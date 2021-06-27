1d691ae61168f9a624ecc955c15caf96b10d66fe
andres duque <totod8@hotmail.com>
2021-06-26 21:55:32 -0700

    mycommit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /*
- * jack_bauer - clock function
- * @void - no return
+ * jack_bauer - check the code for Holberton School students.
+ *
  * Return: Always 0
  */
 void jack_bauer(void)
