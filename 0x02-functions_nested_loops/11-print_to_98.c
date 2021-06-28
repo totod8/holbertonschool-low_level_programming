8c20dfc80822ee644ae6fe810f34cd27051a0b92
andres duque <totod8@hotmail.com>
2021-06-27 18:14:34 -0700

    mycommit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,28 @@
  */
 void print_to_98(int n)
 {
+	int k;
+
+	k = 98;
 	if (n > 98)
 	{
 		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
+		printf("%d", k);
+		printf("\n");
 	}
-		else if (n < 98)
+	else if (n < 98)
 		{
 			for (; n < 98; n++)
 			{
 				printf("%d, ", n);
 			}
-			printf("%d\n",n);
+			printf("%d\n", n);
 		}
+	else
+	{
+		printf("%d\n",n);
+	}
 }
