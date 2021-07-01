0857ec43a1d485327c17ee0a1bdf0b584d22237d
totod8 <totod8@hotmail.com>
2021-07-01 11:34:02 -0700

    mycommit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - display stream
+ * @str: parameter
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i <= 63; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
