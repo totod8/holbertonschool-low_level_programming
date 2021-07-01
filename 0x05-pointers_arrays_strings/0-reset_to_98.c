2cdc7db4cbe8a01eb34c9502b06a015826204fc8
totod8 <totod8@hotmail.com>
2021-06-30 21:36:08 -0700

    mycommit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as paramester
+ * @n: parameter
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
