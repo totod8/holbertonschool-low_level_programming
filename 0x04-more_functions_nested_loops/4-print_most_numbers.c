17da9d01ff356f24ab8375dab24f17345424a42b
totod8 <totod8@hotmail.com>
2021-07-04 18:31:47 -0700

    mycommit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers -  prints the numbers except 2 and 4.
- * @c: print
+ *
  * Retur: no return
  */
 void print_most_numbers(void)
@@ -10,7 +10,11 @@ void print_most_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		if (c != '2' && c != '4')
+		{
 			_putchar(c);
+		}
+	}
 	_putchar('\n');
 }
