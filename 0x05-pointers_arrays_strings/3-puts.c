4c807929cb5ecedd5f3fdc5c55edba2c000220dc
totod8 <totod8@hotmail.com>
2021-07-06 09:08:29 -0700

    mycommit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,17 @@
 void _puts(char *str)
 {
 	int i;
+	int cont = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		cont = cont + 1;
+	}
+	cont = cont - 1;
 
 	for (i = 0; i <= 62; i++)
 	{
-	_putchar(str[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
