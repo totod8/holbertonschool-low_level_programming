b9238b1c35f81c132b2162ad1476471896d01990
totod8 <totod8@hotmail.com>
2021-08-03 12:47:58 -0700

    mycommit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,7 +29,7 @@ typedef struct listint_s
 }	listint_t;
 
 size_t print_listint(const listint_t *h);
-
+int _putchar(char c);
 size_t listint_len(const listint_t *h);
 
 listint_t *add_nodeint(listint_t **head, const int n);
