fd2bd2a79532456fa514b5aec703787b35fb0f1e
totod8 <totod8@hotmail.com>
2021-08-03 16:11:53 -0700

    mycommit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,11 +6,10 @@
  * @index: which node to stop at
  * Return: pointer to the nth node
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	
+
 	i = 0;
 	while (i < index)
 	{
