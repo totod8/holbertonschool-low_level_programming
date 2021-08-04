6b01563367d31ae79e5235a80be70506c586137a
totod8 <totod8@hotmail.com>
2021-08-03 21:41:32 -0700

    mycommit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,9 +27,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 		if (current == NULL)
 			return (-1);
+	}
 		subsequent = current->next;
 		current->next = subsequent->next;
 		free(subsequent);
-	}
-	return (1);
+		return (1);
 }
